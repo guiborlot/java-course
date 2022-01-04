030aa37cc950e27301ad7aacdbe96cc7d3cbe585
Guilherme Borlot Oliveira <guiborlot@edu.unifil.br>
2022-01-04 10:36:09 -0300

    Exercicios while

---
+++ cap-6-estruturas-repetitivas/exercicios-estrutura-repetitiva-while/src/Exercicio2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Exercicio2 {
+
+    //Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema
+    //cartesiano. Para cada ponto escrever o quadrante a que ele pertence. O algoritmo será encerrado quando pelo
+    //menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma).
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe as coordenadas: ");
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        while(x != 0 && y != 0){
+            if(x > 0 && y > 0){
+                System.out.println("primeiro");
+            } else if(x > 0 && y < 0){
+                System.out.println("quarto");
+            } else if(x < 0 && y < 0){
+                System.out.println("terceiro");
+            } else if(x < 0 && y > 0){
+                System.out.println("segundo");
+            }
+            System.out.println("Informe as coordenadas novamente: ");
+            x = sc.nextInt();
+            y = sc.nextInt();
+        }
+
+        sc.close();
+    }
+}

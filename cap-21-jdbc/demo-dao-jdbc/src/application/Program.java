4494e4e0282378b3dc73cbc8a1d81fb51d89d81b
Guilherme Borlot Oliveira <guiborlot@edu.unifil.br>
2022-01-12 11:59:13 -0300

    cap-21-jdbc: implementando findByDepartment()

--- cap-21-jdbc/demo-dao-jdbc/src/application/Program.java
+++ cap-21-jdbc/demo-dao-jdbc/src/application/Program.java
@@ -2,7 +2,11 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
+
+import javax.swing.plaf.SliderUI;
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
@@ -14,5 +18,12 @@ public class Program {
         Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
+
+        System.out.println("\n=== TEST 1: seller findByDepartment ===");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for(Seller obj : list){
+            System.out.println(obj);
+        }
     }
 }

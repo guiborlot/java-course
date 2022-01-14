140294e0c91573516cc41236f0354f60349da3b1
Guilherme Borlot Oliveira <guiborlot@edu.unifil.br>
2022-01-14 10:11:50 -0300

    Department form controller

---
+++ cap-23-proj-javafx/workshop-javafx-jdbc/src/gui/DepartmentFormController.java
@@ -0,0 +1,51 @@
+package gui;
+
+import gui.util.Constraints;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DepartmentFormController implements Initializable {
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private Button btSave;
+
+    @FXML
+    private Button btCancel;
+
+    @FXML
+    private Label labelErrorName;
+
+    @FXML
+    public void onBtSaveAction(){
+        System.out.println("Ta salvando");
+    }
+
+    @FXML
+    public void onBtCancelAction(){
+        System.out.println("Ta cancelando");
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb){
+        initializeNode();
+    }
+
+    private void initializeNode(){
+        Constraints.setTextFieldInteger(txtId);
+        Constraints.setTextFieldMaxLength(txtName, 30);
+
+    }
+}

44a36008fb46535925a2a5a81d18987c9bb8b36d
Guilherme Borlot Oliveira <guiborlot@edu.unifil.br>
2022-01-17 10:48:21 -0300

    OrderStatus enum

--- cap-25-proj-springboot/course/src/main/java/com/example/course/entities/Order.java
+++ cap-25-proj-springboot/course/src/main/java/com/example/course/entities/Order.java
@@ -1,5 +1,6 @@
 package com.example.course.entities;
 
+import com.example.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
@@ -7,7 +8,6 @@ import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
 
-@AllArgsConstructor
 @EqualsAndHashCode
 @Entity
 @Table(name = "tb_order")
@@ -24,11 +24,30 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     @Getter
     private User client;
 
     public Order() {
+    }
+
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+        this.id = id;
+        this.moment = moment;
+        setOrderStatus(orderStatus);
+        this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus){
+        if(orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 }

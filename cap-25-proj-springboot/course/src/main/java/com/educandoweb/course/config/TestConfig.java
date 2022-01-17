facfe2686909a68519c769a63d18a97dbd94cc75
Guilherme Borlot Oliveira <guiborlot@edu.unifil.br>
2022-01-17 14:23:18 -0300

    Payment, one-to-one association

--- cap-25-proj-springboot/course/src/main/java/com/educandoweb/course/config/TestConfig.java
+++ cap-25-proj-springboot/course/src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -3,16 +3,12 @@ package com.educandoweb.course.config;
 import java.time.Instant;
 import java.util.Arrays;
 
+import com.educandoweb.course.entities.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import com.educandoweb.course.entities.Category;
-import com.educandoweb.course.entities.Order;
-import com.educandoweb.course.entities.OrderItem;
-import com.educandoweb.course.entities.Product;
-import com.educandoweb.course.entities.User;
 import com.educandoweb.course.entities.enums.OrderStatus;
 import com.educandoweb.course.repositories.CategoryRepository;
 import com.educandoweb.course.repositories.OrderItemRepository;
@@ -79,6 +75,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
-		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));	
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
 	}
 }

d68ebae971f32ac68fa83f23f9a5b16728da2954
Guilherme Borlot Oliveira <guiborlot@edu.unifil.br>
2022-01-18 14:50:21 -0300

    Criando entity Post com User aninhado

---
+++ cap-26-proj-springmongodb/workshopmongo/src/main/java/com/borlot/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.borlot.workshopmongo.repository;
+
+import com.borlot.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
